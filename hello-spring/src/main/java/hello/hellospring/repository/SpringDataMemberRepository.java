eb4146ecd58e990f8e38c0562b4b27855e152ab2
hyebinppark <gpqls304@naver.com>
2020-10-29 21:24:40 +0900

    feat: spring data member 생성

--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataMemberRepository.java
@@ -1,2 +1,16 @@
-package hello.hellospring.repository;public interface SpringDataMemberRepository {
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SpringDataMemberRepository extends JpaRepository<User, Long> {
+
+    @Override
+    List<User> findAll();
 }
--- hello-spring/src/test/java/hello/hellospring/repository/SpringDataMemberRepositoryTests.java
+++ hello-spring/src/test/java/hello/hellospring/repository/SpringDataMemberRepositoryTests.java
@@ -1,2 +1,27 @@
-package hello.hellospring.repository;public class SpringDataMemberRepositoryTests {
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringDataMemberRepositoryTests {
+
+    @Autowired
+    private SpringDataMemberRepository springDataMemberRepository;
+
+    public SpringDataMemberRepositoryTests(SpringDataMemberRepository springDataMemberRepository) {
+        this.springDataMemberRepository = springDataMemberRepository;
+    }
+
+    @Test
+    public User join() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("test");
+        user.setEmail("test@email.com");
+        return springDataMemberRepository.save(user);
+    }
 }
