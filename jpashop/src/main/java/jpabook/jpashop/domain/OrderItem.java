90b1a6397730fa38ebb5658b4d7a223fa3b77e45
hyebinppark <gpqls304@naver.com>
2020-11-01 20:16:17 +0900

    chore: @XToOne fetch 전략 수정

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -18,7 +18,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
