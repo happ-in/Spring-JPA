08ef44693dd6c916d5b52831736e5bb351b1bf30
hyebinppark <gpqls304@naver.com>
2020-12-06 17:52:31 +0900

    add: 간단한 주문 조회(엔티티 노출)

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne 성능 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/vi/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
