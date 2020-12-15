39384bfb9c4ebbadfe190eb76708616efd04fe7f
hyebinppark <gpqls304@naver.com>
2020-12-16 04:25:06 +0900

    add: 생성자 추가

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -35,6 +35,11 @@ public class Member {
         }
     }
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
