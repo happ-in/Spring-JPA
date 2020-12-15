77cb8a9b0573f42b75f8445420385deed427d7c7
hyebinppark <gpqls304@naver.com>
2020-12-16 04:26:10 +0900

    feat: 메소드에 쿼리 정의 추가

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,7 +2,14 @@ package study.datajpa.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select m from Member m where m.username=:username and m.age=:age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
