67cadea61311017c2245b5939df5003f1cfda5ce
hyebinppark <gpqls304@naver.com>
2020-12-16 19:42:39 +0900

    add: 순수 jpa 페이징

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -41,4 +41,18 @@ public class MemberJpaRepository {
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where age=:age order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long tatalCount(int age) {
+        return em.createQuery("select count(m) from Member m where age=:age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
--- data-jpa/src/test/java/study/datajpa/entity/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberJpaRepositoryTest.java
@@ -88,4 +88,25 @@ class MemberJpaRepositoryTest {
         assertThat(deleteCount).isEqualTo(0);
     }
 
+    @Test
+    public void paging() {
+        //given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        //when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.tatalCount(age);
+
+        //then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+    }
 }
