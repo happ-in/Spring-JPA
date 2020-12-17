5a2af01fc2c1c9decb11d64cf68c1e244b34d6fb
hyebinppark <gpqls304@naver.com>
2020-12-17 22:13:30 +0900

    feat: 페이징과 정렬
    - 매개변수에 Pageable을 넘겨주면 자동으로 페이징 가능
    - 기본 size는 20
    - yml에서 글로벌하게 size 설정 가능
    - @PageableDefault로 size 설정 가능(global보다 우선 순위 높음)

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -3,6 +3,9 @@ package study.datajpa.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +32,15 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i=0; i<100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
