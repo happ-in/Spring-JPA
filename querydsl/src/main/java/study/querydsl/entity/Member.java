9139396b4d5984719f4ecb0321b2aa55627144f2
happ-in <gpqls304@naver.com>
2020-12-20 16:35:04 +0900

    fix: 연관관계 매핑 버그 수정

--- querydsl/src/main/java/study/querydsl/entity/Member.java
+++ querydsl/src/main/java/study/querydsl/entity/Member.java
@@ -14,16 +14,23 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
-
-    private String name;
+    private String username;
     private int age;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @Column(name = "team_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
     private Team team;
 
-    public Member(String name, int age, Team team) {
-        this.name = name;
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
         this.age = age;
         if(team != null) {
             changeTeam(team);
